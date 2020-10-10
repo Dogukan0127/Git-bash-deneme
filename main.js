ef8801c8edd1d94aad5b52783c2a4cbc25be4f36
Dogukan0127 <dogukangs2005@gmail.com>
2020-10-10 15:30:25 +0300

    some write code

---
+++ main.js
@@ -0,0 +1,10 @@
+let cümle = "Selamlar Sevgili Arkdaşlar :)";
+console.log(cümle);
+
+let cümleisobject = Number(cümle);
+console.log(cümleisobject);
+
+
+for (let i =15;i<72;i++){
+    console.log(i);
+}
